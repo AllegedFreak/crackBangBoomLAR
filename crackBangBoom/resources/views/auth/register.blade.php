1b6eaca8eb4add0b1d24da732a53fa973c7dae6b
Marcos <marcoscav@gmail.com>
2018-12-19 16:32:00 -0300

    correciones

--- crackBangBoom/resources/views/auth/register.blade.php
+++ crackBangBoom/resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+<!-- @extends('layouts.main')
 
 @section('content')
 <div class="container-fluid">
@@ -28,8 +28,8 @@
                               </div>
                           </div> -->
 
-                          <div class="form-group row">
-                              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                          <!-- <div class="form-group row">
+                              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Maaaaail') }}</label>
 
                               <div class="col-md-6">
                                   <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -90,4 +90,4 @@
 <script src="/js/validformssubmit.js"></script>
 
 
-@endsection
+@endsection --> -->
